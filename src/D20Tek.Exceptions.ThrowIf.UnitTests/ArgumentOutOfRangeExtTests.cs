ed62a2a76cc89d7fcc47340910dfa9ea55fefd52
Pedro Silva <DarthPedro@gmail.com>
2025-05-17 16:37:28 -0700

    Added unit test project for Exceptions.ThrowIf library. Added full set of tests for ArgumentOutOfRangeExceptionExt methods.

---
+++ src/D20Tek.Exceptions.ThrowIf.UnitTests/ArgumentOutOfRangeExtTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Exceptions.ThrowIf.UnitTests;
+
+[TestClass]
+public sealed class ArgumentOutOfRangeExtTests
+{
+    [TestMethod]
+    [DataRow(5, 1, 10)]
+    [DataRow(1, 1, 10)]
+    [DataRow(10, 1, 10)]
+    [DataRow(5, 5, 5)]
+    public void ThrowIfOutOfRange_DoesNotThrowException(int check, int min, int max)
+    {
+        // arrange
+
+        // act
+        ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRange(check, min, max);
+
+        // assert
+    }
+
+    [TestMethod]
+    [DataRow(15, 1, 10)]
+    [DataRow(1, 10, 20)]
+    [DataRow(5, 10, 1)]
+    public void ThrowIfOutOfRange_ThrowsException(int check, int min, int max)
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentOutOfRangeException>([ExcludeFromCodeCoverage] () =>
+            ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRange(check, min, max));
+
+        Assert.AreEqual(nameof(check), ex.ParamName);
+    }
+
+    [TestMethod]
+    public void ThrowIfOutOfRangeFloat_WithMaxValue_DoesNotThrowException()
+    {
+        // arrange
+        var check = 3.14f;
+
+        // act
+        ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRange(check, 1.0f, 10.0f);
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIfOutOfRangeFloat_WithGreaterThanMax_ThrowException()
+    {
+        // arrange
+        var check = 10.35f;
+
+        // act
+        Assert.ThrowsException<ArgumentOutOfRangeException>([ExcludeFromCodeCoverage] () =>
+            ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRange(check, 1.0f, 10.0f));
+    }
+
+    [TestMethod]
+    public void ThrowIfOutOfRangeExclusive_WithValueInRange_DoesNotThrowException()
+    {
+        // arrange
+        var check = 5;
+
+        // act
+        ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRangeExclusive(check, 1, 10);
+
+        // assert
+    }
+
+    [TestMethod]
+    [DataRow(1, 1, 10)]
+    [DataRow(10, 1, 10)]
+    [DataRow(15, 1, 10)]
+    [DataRow(1, 10, 20)]
+    [DataRow(5, 5, 5)]
+    [DataRow(5, 10, 1)]
+    public void ThrowIfOutOfRangeExclusive_ThrowsException(int check, int min, int max)
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentOutOfRangeException>([ExcludeFromCodeCoverage] () =>
+            ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRangeExclusive(check, min, max));
+
+        Assert.AreEqual(nameof(check), ex.ParamName);
+    }
+
+    [TestMethod]
+    public void ThrowIfOutOfRangeDecimal_WithMaxValue_DoesNotThrowException()
+    {
+        // arrange
+        var check = 3.14M;
+
+        // act
+        ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRangeExclusive(check, 1.0M, 10.0M);
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIfOutOfRangeDecimal_WithGreaterThanMax_DoesNotThrow()
+    {
+        // arrange
+        var check = 10.35m;
+
+        // act
+        Assert.ThrowsException<ArgumentOutOfRangeException>([ExcludeFromCodeCoverage] () =>
+            ArgumentOutOfRangeExceptionExt.ThrowIfOutOfRange(check, 1.0m, 10.0m));
+    }
+}
---
+++ src/D20Tek.Exceptions.ThrowIf/ArgumentOutOfRangeExt.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace D20Tek.Exceptions.ThrowIf;
+
+public class ArgumentOutOfRangeExceptionExt : ArgumentOutOfRangeException
+{
+    public static void ThrowIfOutOfRange<T>(
+        T value, T min, T max, [CallerArgumentExpression(nameof(value))] string paramName = "none")
+        where T : IComparable<T>
+    {
+        if (min.CompareTo(max) > 0) 
+            throw new ArgumentOutOfRangeException(paramName, string.Format(ArgumentOutOfRange_MinMax, min, max));
+
+        if (value.CompareTo(min) < 0 || value.CompareTo(max) > 0)
+            ThrowOutOfRange(value, min, max, paramName, ArgumentOutOfRange_MustBeInRange);
+    }
+
+    public static void ThrowIfOutOfRangeExclusive<T>(
+        T value, T min, T max, [CallerArgumentExpression(nameof(value))] string paramName = "none")
+        where T : IComparable<T>
+    {
+        if (min.CompareTo(max) > 0)
+            throw new ArgumentOutOfRangeException(paramName, string.Format(ArgumentOutOfRange_MinMax, min, max));
+
+        if (value.CompareTo(min) <= 0 || value.CompareTo(max) >= 0)
+            ThrowOutOfRange(value, min, max, paramName, ArgumentOutOfRange_MustBeInRangeExclusive);
+    }
+
+    public const string ArgumentOutOfRange_MinMax =
+        "The value for min '{0}' must be less than or equal to max '{1}'.";
+    public const string ArgumentOutOfRange_MustBeInRange =
+        "The value '{0}' for parameter '{1}' must be in the range [{2}, {3}].";
+    public const string ArgumentOutOfRange_MustBeInRangeExclusive =
+        "The value '{0}' for parameter '{1}' must be in the range ({2}, {3}) - excluding the min max.";
+
+    [DoesNotReturn]
+    private static void ThrowOutOfRange<T>(T value , T min, T max, string paramName, string messageFormat) =>
+        throw new ArgumentOutOfRangeException(
+            paramName,
+            value,
+            string.Format(messageFormat, value, paramName, min, max));
+}
