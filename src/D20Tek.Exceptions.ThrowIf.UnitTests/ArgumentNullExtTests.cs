145dbb844247a27e2ddd4a157ca48cc7094344ba
Pedro Silva <DarthPedro@gmail.com>
2025-05-17 17:14:23 -0700

    Added ArgumentNullExceptionExt class with ThrowIfNullOrEmpty for IEnumerable and IDictionary based collections. Added unit tests to cover those methods.

---
+++ src/D20Tek.Exceptions.ThrowIf.UnitTests/ArgumentNullExtTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Exceptions.ThrowIf.UnitTests;
+
+[TestClass]
+public class ArgumentNullExtTests
+{
+    [TestMethod]
+    public void ThrowIfNullOrEmpty_WithFullArray_DoesNotThrowException()
+    {
+        // arrange
+        var list = new int[] { 3, 7, 5, 9 };
+
+        // act
+        ArgumentNullExceptionExt.ThrowIfNullOrEmpty(list);
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIfNullOrEmpty_WithEmptyArray_ThrowsException()
+    {
+        // arrange
+        int[] list = [];
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentException>([ExcludeFromCodeCoverage] () =>
+            ArgumentNullExceptionExt.ThrowIfNullOrEmpty(list));
+
+        Assert.AreEqual(nameof(list), ex.ParamName);
+    }
+
+    [TestMethod]
+    public void ThrowIfNullOrEmpty_WithNullList_ThrowsException()
+    {
+        // arrange
+        List<string>? list = null;
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentNullException>([ExcludeFromCodeCoverage] () =>
+            ArgumentNullExceptionExt.ThrowIfNullOrEmpty(list));
+
+        Assert.AreEqual(nameof(list), ex.ParamName);
+    }
+
+    [TestMethod]
+    public void ThrowIfNullOrEmpty_WithFullDictionary_DoesNotThrowException()
+    {
+        // arrange
+        var dict = new Dictionary<int, string>
+        {
+            { 3, "Three" },
+            { 7, "Seven" },
+            { 5, "Five" }
+        };
+
+        // act
+        ArgumentNullExceptionExt.ThrowIfNullOrEmpty(dict);
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIfNullOrEmpty_WithEmptyDictionary_ThrowsException()
+    {
+        // arrange
+        var dict = new Dictionary<int, string>();
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentException>([ExcludeFromCodeCoverage] () =>
+            ArgumentNullExceptionExt.ThrowIfNullOrEmpty(dict));
+
+        Assert.AreEqual(nameof(dict), ex.ParamName);
+    }
+
+    [TestMethod]
+    public void ThrowIfNullOrEmpty_WithNullDictionary_ThrowsException()
+    {
+        // arrange
+        IDictionary<int, string>? dict = null;
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentNullException>([ExcludeFromCodeCoverage] () =>
+            ArgumentNullExceptionExt.ThrowIfNullOrEmpty(dict!));
+
+        Assert.AreEqual(nameof(dict), ex.ParamName);
+    }
+}
---
+++ src/D20Tek.Exceptions.ThrowIf/ArgumentNullExt.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace D20Tek.Exceptions.ThrowIf;
+
+public class ArgumentNullExceptionExt : ArgumentNullException
+{
+    public static void ThrowIfNullOrEmpty<T>(
+        IEnumerable<T>? value, [CallerArgumentExpression(nameof(value))] string paramName = "none")
+    {
+        ArgumentNullException.ThrowIfNull(value, paramName);
+        if (value.Any() is false)
+            throw new ArgumentException(string.Format(CollectionEmpty, paramName), paramName);
+    }
+
+    public static void ThrowIfNullOrEmpty<TKey, TValue>(
+        IDictionary<TKey, TValue> value, [CallerArgumentExpression(nameof(value))] string paramName = "none")
+    {
+        ArgumentNullException.ThrowIfNull(value, paramName);
+        if (value.Any() is false)
+            throw new ArgumentException(string.Format(DictionaryEmpty, paramName), paramName);
+    }
+
+    private const string CollectionEmpty = "The collection with parameter name '{0}' cannot be empty.";
+    private const string DictionaryEmpty = "The dictionary with parameter name '{0}' cannot be empty.";
+}
