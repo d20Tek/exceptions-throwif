bf7f8b4fd7050b953c89879a2e24e87ca6ebbb2d
Pedro Silva <DarthPedro@gmail.com>
2025-05-17 17:31:47 -0700

    Added exception check for ThrowIfInvalidEnum. Added unit tests.

---
+++ src/D20Tek.Exceptions.ThrowIf.UnitTests/InvalidEnumArgumentExtTests.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Exceptions.ThrowIf.UnitTests;
+
+[TestClass]
+public class InvalidEnumArgumentExtTests
+{
+    public enum TestType
+    {
+        None,
+        Some,
+        Examples
+    };
+
+    [TestMethod]
+    public void ThrowIfInvalidEnum_WithValidEnumValue_DoesNotThrowException()
+    {
+        // arrange
+        var test = TestType.Some;
+
+        // act
+        InvalidEnumArgumentExceptionExt.ThrowIfInvalidEnum(test);
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIfInvalidEnum_WithInvalidEnumValue_ThrowsException()
+    {
+        // arrange
+        var test = (TestType)99;
+
+        // assert
+        Assert.ThrowsException<InvalidEnumArgumentException>([ExcludeFromCodeCoverage] () =>
+            InvalidEnumArgumentExceptionExt.ThrowIfInvalidEnum(test));
+    }
+}
---
+++ src/D20Tek.Exceptions.ThrowIf/InvalidEnumArgumentExt.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace D20Tek.Exceptions.ThrowIf;
+
+public class InvalidEnumArgumentExceptionExt : InvalidEnumArgumentException
+{
+    public static void ThrowIfInvalidEnum<TEnum>(
+        TEnum value, [CallerArgumentExpression(nameof(value))] string paramName = "none")
+        where TEnum : struct, Enum
+    {
+        if (Enum.IsDefined(value) is false)
+            throw new InvalidEnumArgumentException(
+                string.Format(InvalidEnumMessage, paramName, typeof(TEnum).Name, value));
+    }
+
+    private const string InvalidEnumMessage = "The parameter {0} has an invalid value for enum type {1}: {2}.";
+}
