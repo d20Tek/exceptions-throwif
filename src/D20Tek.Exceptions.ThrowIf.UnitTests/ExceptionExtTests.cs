e1915250aba511b62ab2f0781f6c0b835d5583fd
Pedro Silva <DarthPedro@gmail.com>
2025-05-19 14:23:47 -0700

    Added Exception extensions to ThrowIf based on condition or Func that returns a bool... for general ThrowIf usage. Added unit tests.

---
+++ src/D20Tek.Exceptions.ThrowIf.UnitTests/ExceptionExtTests.cs
@@ -0,0 +1,118 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Exceptions.ThrowIf.UnitTests;
+
+[TestClass]
+public class ExceptionExtTests
+{
+    [TestMethod]
+    public void ThrowIf_DoesNotThrow_WhenConditionIsFalse()
+    {
+        // arrange
+
+        // act
+        ExceptionExt.ThrowIf<InvalidOperationException>(false, "This should not be thrown");
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIf_ThrowsArgumentException_WhenConditionIsTrue()
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentException>([ExcludeFromCodeCoverage] () =>
+            ExceptionExt.ThrowIf<ArgumentException>(true, "Invalid argument provided"));
+    }
+
+    [TestMethod]
+    public void ThrowIf_ThrowsInvalidOperationException_WhenConditionIsTrue()
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<InvalidOperationException>([ExcludeFromCodeCoverage] () =>
+            ExceptionExt.ThrowIf<InvalidOperationException>(true, "Invalid operation"));
+    }
+
+    [TestMethod]
+    public void ThrowIf_ThrowsMissingMethodException_WhenExceptionTypeHasNoStringConstructor()
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<MissingMethodException>([ExcludeFromCodeCoverage] () =>
+            ExceptionExt.ThrowIf<ExceptionWithoutStringConstructor>(true, "No message constructor"));
+    }
+
+    // Helper class for testing fallback failure behavior
+    [ExcludeFromCodeCoverage]
+    private class ExceptionWithoutStringConstructor : Exception
+    {
+        public ExceptionWithoutStringConstructor() { }
+    }
+
+    [TestMethod]
+    public void ThrowIfNot_DoesNotThrow_WhenConditionIsTrue()
+    {
+        // arrange
+
+        // act
+        ExceptionExt.ThrowIfNot<InvalidOperationException>(true, "This should not be thrown");
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIfNot_ThrowsArgumentException_WhenConditionIsFalse()
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentException>([ExcludeFromCodeCoverage] () =>
+            ExceptionExt.ThrowIfNot<ArgumentException>(false, "Invalid argument provided"));
+    }
+
+    [TestMethod]
+    public void ThrowIf_DoesNotThrow_WhenFuncConditionIsFalse()
+    {
+        // arrange
+
+        // act
+        ExceptionExt.ThrowIf<InvalidOperationException>(() => false, "This should not be thrown");
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIf_ThrowsArgumentException_WhenFuncConditionIsTrue()
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentException>([ExcludeFromCodeCoverage] () =>
+            ExceptionExt.ThrowIf<ArgumentException>(() => true, "Invalid argument provided"));
+    }
+
+    [TestMethod]
+    public void ThrowIfNot_DoesNotThrow_WhenFuncConditionIsTrue()
+    {
+        // arrange
+
+        // act
+        ExceptionExt.ThrowIfNot<InvalidOperationException>(() => true, "This should not be thrown");
+
+        // assert
+    }
+
+    [TestMethod]
+    public void ThrowIfNot_ThrowsArgumentException_WhenFuncConditionIsFalse()
+    {
+        // arrange
+
+        // assert
+        var ex = Assert.ThrowsException<ArgumentException>([ExcludeFromCodeCoverage] () =>
+            ExceptionExt.ThrowIfNot<ArgumentException>(() => false, "Invalid argument provided"));
+    }
+}
---
+++ src/D20Tek.Exceptions.ThrowIf/ExceptionExt.cs
@@ -0,0 +1,27 @@
+﻿namespace D20Tek.Exceptions.ThrowIf;
+
+public class ExceptionExt : Exception
+{
+    public static void ThrowIf<TException>(bool condition, string message)
+        where TException : Exception
+    {
+        if (condition)
+        {
+            var exception = (TException?)Activator.CreateInstance(typeof(TException), message);
+            throw exception!;
+        }
+    }
+
+    public static void ThrowIfNot<TException>(bool condition, string message)
+        where TException : Exception =>
+        ThrowIf<TException>(!condition, message);
+
+
+    public static void ThrowIf<TException>(Func<bool> condition, string message)
+        where TException : Exception =>
+        ThrowIf<TException>(condition(), message);
+
+    public static void ThrowIfNot<TException>(Func<bool> condition, string message)
+        where TException : Exception =>
+        ThrowIf<TException>(!condition(), message);
+}
